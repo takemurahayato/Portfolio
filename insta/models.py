e411ce3b016b686d3eb3126512fda3a3ec2c71d7
takemura hayato <you@example.com>
2021-06-11 01:04:19 +0900

    userモデル作成

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    mail = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + self.mail + self.password
