6792634909d9368b09717b3ddeeb2c228224bccf
Shogo1811 <shogo.n.1118@gmail.com>
2021-06-23 19:48:50 +0900

    ログイン機能実装

--- insta/urls.py
+++ insta/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.IndexView.as_view(), name="top"),
     path("login", views.MyLoginView.as_view(), name="login"),
     path("logout", views.MyLogoutView.as_view(), name="logout"),
 ]
